835c52edfb7eda22d0d25787527a5fd3a0b63adb
lilybarrett <lily@barrett.us>
2015-12-03 14:50:03 -0500

    removed link and added infinite scrolling

--- public/js/sprouts.js
+++ public/js/sprouts.js
@@ -1,21 +1,23 @@
 // YOUR CODE GOES HERE
 var page_num = 1;
 
-$(".more-sprouts").on("click", function(event) {
-  event.preventDefault();
+// $(".more-sprouts").on("scroll", function(event) {
+  $(window).scroll(function() {
+    if($(window).scrollTop() == $(document).height() - $(window).height()) {
+      page_num += 1;
+      var request = $.ajax({
+        method: "GET",
+        url: "/tweets.json?page=" + page_num
+      });
 
-  page_num += 1;
-  var request = $.ajax({
-    method: "GET",
-    url: "/tweets.json?page=" + page_num
-  });
+      request.done(function(data) {
+      data.forEach(function(tweet) {
+        $('.tweets').append("<li class='tweet'>" + "<div class='body'>" + tweet['text'] + "</div>" + "<div class='user'>" + tweet['username'] + "</div>" + "</li>");
+      });
 
-  request.done(function(data) {
-    // var newTweets = data["tweets"].val();
-    data.forEach(function(tweet) {
-      $('.tweets').append("<li class='tweet'>" + "<div class='body'>" + tweet['text'] + "</div>" + "<div class='user'>" + tweet['username'] + "</div>" + "</li>");
-    })
+    });
 
-  });
+
+  };
 
 });
